983127e0cfe76e1d75709428261880f5f3617218
Daniel Orija <danielorijadribo@gmail.com>
2023-11-07 15:48:14 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/index.html",
+    "./pages/admin/*.{html,js}",
+    "./pages/student/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
